f70d4c770548a61b32e1076949b50bda4a9e95e5
anweshaghosh12345 <61232824+anweshaghosh12345@users.noreply.github.com>
2025-05-12 01:01:33 +0530

    all

--- src/Arrays/MargeSortedArrays.java
+++ src/Arrays/MargeSortedArrays.java
@@ -9,6 +9,15 @@ public class MargeSortedArrays {
             nums1[i]=nums2[k];
             k++;
         }
+        for (int i = 0; i <nums1.length ; i++) {
+            for (int j = i+1; j < nums1.length; j++) {
+                if(nums1[i]>nums1[j]){
+                    int temp=nums1[j];
+                    nums1[j]=nums1[i];
+                    nums1[i]=temp;
+                }
+            }
+        }
 
         System.out.println(Arrays.toString(nums1));
     }
