604e289f5bdc2fa252f594d1eaf05dd7b98a4c21
anweshaghosh12345 <61232824+anweshaghosh12345@users.noreply.github.com>
2025-05-12 00:45:04 +0530

    all

--- src/Arrays/MargeSortedArrays.java
+++ src/Arrays/MargeSortedArrays.java
@@ -1,5 +1,21 @@
 package Arrays;
 
+import java.util.Arrays;
+
 public class MargeSortedArrays {
-    
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int k=0;
+        for (int i = m; i <nums1.length ; i++) {
+            nums1[i]=nums2[k];
+            k++;
+        }
+
+        System.out.println(Arrays.toString(nums1));
+    }
+    public static void main(String []args){
+        int[]nums1=new int[]{1,2,3,0,0,0};
+        int[]nums2=new int[]{2,5,6};
+        merge(nums1,3,nums2,3);
+        System.out.println(Arrays.toString(nums1));
+    }
 }
--- src/module-info.java
+++ src/module-info.java
@@ -1,2 +1,3 @@
 module leetcode.New {
+    requires jdk.compiler;
 }
